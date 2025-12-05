0e43532930badd3a3e8e7b97f717f7f67b208954
ayoubarich-dev <ayoubarich36@gmail.com>
2025-12-05 16:52:09 +0100

    last commit

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-"""
-Audio Compressor - Point d'entrée de l'application
-Compresseur audio utilisant Delta Encoding + RLE + Huffman
-"""
-
 import sys
 from pathlib import Path
 
-# Ajouter src au path
+
 src_path = Path(__file__).parent / "src"
 sys.path.insert(0, str(src_path))
 
@@ -15,17 +10,14 @@ from gui.main_window import AudioCompressorWindow
 
 
 def main():
-    """Lance l'application Audio Compressor"""
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
     app.setApplicationName("Audio Compressor")
     app.setOrganizationName("AudioCompressor")
     
     window = AudioCompressorWindow()
-    window.show()
-    
+    window.show()  
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
